b4d8116b89869a61d4a56aa2473027a9a19f4146
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-09-29 22:25:05 +0530

    added login asdasdasd

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const express = require('express'),
     flash = require('express-flash'),
     expressLayouts = require('express-ejs-layouts'),
     mongoose = require('mongoose');
+    nodemailer = require('nodemailer')
+    twilio = require('twilio')
 
 //routes
 const landing = require('./routers/landingRouter.js');
@@ -47,6 +49,18 @@ app.use(passport.session())
 //main
 app.use('/', landing)
 
+app.get("/bakwaspage", (req,res) => {
+    res.render('bakwas.ejs')
+})
+app.post("/bakwaspage", (req,res) => {
+    sendMail()
+    res.render('bakwas.ejs')
+})
+app.post("/sms", (req,res) => {
+    sendMessage()
+    res.render('bakwas.ejs')
+})
+
 app.get('/404', (req, res) => {
     res.render('404', { user: req.user })
 });
@@ -54,3 +68,40 @@ app.get('/404', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
+
+
+var client = new twilio('AC584008d51aecc9ecbf624de99a20b074', '5e3960ed1cb85c8d47488aa61237826b');
+
+function sendMessage() {
+    client.messages.create({
+        to: '+918851334909',
+        from: '(619) 727-6302',
+        body: 'Hello from Twilio!'
+      });
+}
+
+var transporter = nodemailer.createTransport({
+  service: 'outlook',
+  auth: {
+    user: 'bhavit.grover@ais.amity.edu',
+    pass: '@FAD1B41S'
+  }
+
+
+});
+async function sendMail() {
+    var mailOptions = {
+        from: 'bhavit.grover@ais.amity.edu',
+        to: 'groverbhavit@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });  
+}
