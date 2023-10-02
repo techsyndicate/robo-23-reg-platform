d173f3d0842b266e0092551962b60c3d944856f8
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-10-02 17:00:09 +0530

    whastapp

--- routers/inviteRouter.js
+++ routers/inviteRouter.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 const sendMail = require('../utils/mailHelper');
+const { Client } = require('whatsapp-web.js');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
 
 router.get('/', (req, res) => {
     res.render('invite', { user: req.user })
@@ -18,7 +39,7 @@ router.post('/', urlencodedParser, (req,res) => {
     for (var i = 0; i < emails.length; i++) {
         sendMail(emails[i], "hello", "this is the text", "<h1>Hello there</h1>")
     }
-    res.send('EMAILS SENT'); 
+    res.send('EMAILS SENT');
 })
 
 //export router
