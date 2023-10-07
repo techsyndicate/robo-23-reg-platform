b6d282be2f9f5fdc226c70d34126d3a886a08a05
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-10-07 17:41:29 +0530

    added admin pages

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -13,9 +13,30 @@ router.get('/schoolData', async (req, res) => {
         if (schoolDetails[i].regType === "school") {
             schools.push(schoolDetails[i].school);
         }
+        if (schoolDetails[i].regType === "indi") {
+            schools.push(schoolDetails[i].indi);
+        }
     }
-    console.log(schools)
+    // console.log(schoolDetails)
     res.render('schoolData', {  user: req.user, schools  })
+})
+
+router.post('/schoolData/:id', async (req, res) => {
+    var id = req.params.id
+    console.log(id)
+    const allUsers = await User.find({})
+    var toSend;
+    for (let i = 0; i < allUsers.length; i++) {
+        if (allUsers[i].regType === "school") {
+            toSend = allUsers[i].school
+        }
+        if (allUsers[i].regType === "indi") {
+            toSend = allUsers[i].indi
+        }
+    }
+    console.log(toSend)
+    // console.log(requiredUser)
+    res.render('checkInSchool', {user: toSend})
 })
 
 //export router
