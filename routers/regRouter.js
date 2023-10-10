8409baf16bf3dabf6910ae7d45612f19591eb58b
gamer-1478 <aayushgarg2006@gmail.com>
2023-10-10 18:46:27 +0530

    temp fix

--- routers/regRouter.js
+++ routers/regRouter.js
@@ -63,11 +63,13 @@ router.post('/school', async (req, res, next) => {
       teamCreateHandle(newUser.school.schoolName)
       await newUser.save().then(async (user) => {
         console.log(user)
-        discoIt(JSON.stringify(user))
+        // discoIt(JSON.stringify(user))
         sendMail(schoolEmail, "Registration for Robotronics 2023", "", await ejs.renderFile(__dirname + "/../views/reg-email.ejs", { userId : schoolEmail, pass: password, token: discordCode }))
         sendMail(clubEmail, "Registration for Robotronics 2023", "", await ejs.renderFile(__dirname + "/../views/reg-email.ejs", { userId: schoolEmail, pass: password, token: discordCode }))
         sendMail(teacherEmail, "Registration for Robotronics 2023", "", await ejs.renderFile(__dirname + "/../views/reg-email.ejs", { userId: schoolEmail, pass: password, token: discordCode }))
-      }).catch(err => discoIt(JSON.stringify(err)))
+      }).catch(err => //discoIt(JSON.stringify(err))
+      console.log(err)
+      )
       await loginUser(req, res, next)
     })
   );
